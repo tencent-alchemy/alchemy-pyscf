03a064831f7787e937a86cb54fa776d3a83fa4c3
Qiming Sun <osirpt.sun@gmail.com>
2019-10-08 13:24:18 -0700

    Add conda env yaml

--- examples/01-run_qm9_property.py
+++ examples/01-run_qm9_property.py
@@ -7,7 +7,7 @@ Computing the 15 properties reported in QM9 paper
 from alchmeypyscf import qm9_properties
 
 # Computing QM9 properties for ethanol
-
-results = qm9_properties('CCO')
+smiles = 'CCO'
+results = qm9_properties(smiles)
 for k, v in results.items():
     print(k, v)
